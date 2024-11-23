4b6c037857649276adbfa645e3e5bf867fcb4234
Soma Lucz <luczsoma@gmail.com>
2024-11-23 13:01:56 +0100

    Update

--- process.py
+++ process.py
@@ -1,33 +1,40 @@
-from argparse import ArgumentParser
 from csv import reader, writer
 from os import scandir
 from os.path import join, isfile
+from pathlib import Path
 
 
-def main() -> None:
-    parser = ArgumentParser()
-    parser.add_argument("isp", choices=["telekom", "dravanet"])
-    args = parser.parse_args()
-    isp = args.isp
+def main():
+    for isp in ["telekom", "dravanet"]:
+        isp_raw_dir_path = Path(join("raw", isp))
+        isp_processed_dir_path = Path(join("processed", isp))
 
-    input_files = [f for f in scandir(join("raw", isp)) if isfile(f)]
-    for input_file in input_files:
-        with open(input_file, "rt", encoding="utf-8") as input:
-            csv_reader = reader(input)
+        if not isp_raw_dir_path.exists():
+            print(f'raw logs for ISP "{isp}" not found')
+            continue
 
-            with open(
-                join("processed", isp, input_file.name), "wt", encoding="utf-8"
-            ) as output_file:
-                csv_writer = writer(output_file, lineterminator="\n")
-                for row in csv_reader:
-                    if row[0] == "Mtr_Version":
-                        # header row
-                        continue
+        isp_raw_dir_path.mkdir(parents=True, exist_ok=True)
+        input_files = [f for f in scandir(isp_raw_dir_path) if isfile(f)]
+        for input_file in input_files:
+            with open(input_file, "rt", encoding="utf-8") as input:
+                csv_reader = reader(input)
 
-                    # remove mtr version
-                    row.pop(0)
+                isp_processed_dir_path.mkdir(parents=True, exist_ok=True)
+                with open(
+                    join(isp_processed_dir_path, input_file.name),
+                    "wt",
+                    encoding="utf-8",
+                ) as output_file:
+                    csv_writer = writer(output_file, lineterminator="\n")
+                    for row in csv_reader:
+                        # remove header rows
+                        if row[0] == "Mtr_Version":
+                            continue
 
-                    csv_writer.writerow(row)
+                        # remove mtr version from rows
+                        row.pop(0)
+
+                        csv_writer.writerow(row)
 
 
 if __name__ == "__main__":
